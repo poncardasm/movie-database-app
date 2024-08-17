963b40adc564d5a2fa74c6066bed2c64c116526a
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-08-17 03:21:36 +0300

    Add page router and highlight active link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Highlight active link
+function highlightActiveLink() {
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Init app
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
