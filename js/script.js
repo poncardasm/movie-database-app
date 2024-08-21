80775394283a3d7fa9d90386f9555be71ed49b3b
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-08-21 13:09:31 +0300

    refactor: Dynamically generate popular TV show cards from API response

--- js/script.js
+++ js/script.js
@@ -53,6 +53,42 @@ async function displayPopularMovies() {
   });
 }
 
+// Display popular TV Shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  console.log(results);
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            alt="${show.name}"
+            class="card-img-top"
+          />`
+              : `<img
+            src="images/no-image.jpg"
+            alt="${show.name}"
+            class="card-img-top"
+          />`
+          }
+        </a>
+
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+    `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
@@ -79,7 +115,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
