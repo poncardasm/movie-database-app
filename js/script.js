c4d07917b1c650dc833a0f9583aa2c2c47d22044
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-08-25 23:06:25 +0300

    refactor: Update movie details page to dynamically display movie information
    - Added function displayMovieDetails() to fetch and display movie details based on the movie ID in the URL query parameter
    - Created HTML template for movie details and populated it with data from the API response
    - Implemented formatting functions formatReleaseDate() and formatNumberWithComma() to format release dates and numbers with commas
    - Updated script.js to call displayMovieDetails() on the movie-details.html page

--- js/script.js
+++ js/script.js
@@ -45,12 +45,83 @@ async function displayPopularMovies() {
         <div class="card-body">
           <h5 class="card-title">${movie.title}</h5>
           <p class="card-text">
-            <small class="text-muted">Released: ${movie.release_date}</small>
+            <small class="text-muted">Released: ${formatReleaseDate(
+              movie.release_date
+            )}</small>
           </p>
         </div>
     `;
     document.querySelector('#popular-movies').appendChild(div);
   });
+}
+
+// Display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            alt="${movie.title}"
+            class="card-img-top"
+          />`
+              : `<img
+            src="images/no-image.jpg"
+            alt="${movie.title}"
+            class="card-img-top"
+          />`
+          }
+          </div>
+
+          <div>
+            <h1>${movie.title}</h1>
+            <p><i class="fas fa-star text-primary"></i> ${movie.vote_average.toFixed(
+              1
+            )} / 10</p>
+            <p class="text-muted">Release Date: ${formatReleaseDate(
+              movie.release_date
+            )}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $ ${formatNumberWithComma(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $ ${formatNumberWithComma(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+            <li><span class="text-secondary">Production:</span> ${movie.production_companies
+              .map((company) => `${company.name}`)
+              .join(', ')}
+            </li>
+          </ul>
+        </div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
 }
 
 // Display popular TV Shows
@@ -81,7 +152,9 @@ async function displayPopularShows() {
         <div class="card-body">
           <h5 class="card-title">${show.name}</h5>
           <p class="card-text">
-            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            <small class="text-muted">Air Date: ${formatReleaseDate(
+              show.first_air_date
+            )}</small>
           </p>
         </div>
     `;
@@ -95,6 +168,19 @@ function showSpinner() {
 
 function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
+}
+
+// Format release date
+function formatReleaseDate(releaseDateStr) {
+  const releaseDate = new Date(releaseDateStr);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedReleaseDate = releaseDate.toLocaleDateString('en-US', options);
+  return formattedReleaseDate;
+}
+
+// Format number with comma
+function formatNumberWithComma(number) {
+  return new Intl.NumberFormat('en-US').format(number);
 }
 
 // Highlight active link
@@ -114,11 +200,11 @@ function init() {
     case '/index.html':
       displayPopularMovies();
       break;
+    case '/movie-details.html':
+      displayMovieDetails();
+      break;
     case '/shows.html':
       displayPopularShows();
-      break;
-    case '/movie-details.html':
-      console.log('Movie Details');
       break;
     case '/tv-details.html':
       console.log('TV Details');
